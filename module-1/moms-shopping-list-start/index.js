4d781e0fd018dd491657911fae8db3a9fcedf309
Kit Krajeski <kitkrajeski@gmail.com>
2024-02-12 09:25:52 -0700

    moms-shopping-list(update)

--- module-1/moms-shopping-list-start/index.js
+++ module-1/moms-shopping-list-start/index.js
@@ -10,6 +10,46 @@ const listContainer = document.getElementById("list");
 
 userInput.addEventListener("submit", (event) => {
     event.preventDefault();
-    const listInput = document.getElementById("title").value;
-    
+    const inputField = document.getElementById("title");
+    const listInput = inputField.value
+    const newItem = document.createElement("li");
+    const newListItem = document.createElement("div")
+    newListItem.textContent = listInput;
+    // newItem.insertAdjacentHTML("afterbegin", "<div>".concat(listInput, "</div>"))
+    // newItem.classList.add("list");
+    const buttonEdit = document.createElement("button");
+    buttonEdit.textContent = "Edit";
+    buttonEdit.classList.add("editButton");
+    const buttonDel = document.createElement("button");
+    buttonDel.textContent = "X";
+    buttonDel.classList.add("deleteButton");
+    newItem.appendChild(newListItem);
+    newItem.appendChild(buttonEdit);
+    newItem.appendChild(buttonDel);
+    listContainer.appendChild(newItem);
+    inputField.value = "";
 });
+
+listContainer.addEventListener("click", event => {
+    const clickedElement = event.target;
+    if (clickedElement.classList.contains("deleteButton")) {
+        const listItem = clickedElement.parentNode;
+        listItem.remove();
+    } else if (clickedElement.classList.contains("editButton")) {
+        const listItem = clickedElement.parentNode;
+        const currentText = listItem.childNodes[0].textContent.trim();
+        const inputField = document.createElement("input");
+        const buttonSub = document.createElement("button")
+        buttonSub.textContent = "Submit";
+        inputField.type = "text";
+        inputField.value = currentText;
+        inputField.textContent = "";
+        listItem.appendChild(inputField);
+        listItem.appendChild(buttonSub);
+        buttonSub.addEventListener("click", event => {
+            listItem.childNodes[0].textContent = inputField.value;
+            inputField.remove();
+            buttonSub.remove();
+        })
+    }
+})

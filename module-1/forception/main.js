6f04e77bf8c9c908f03daff474b21a5842948457
Kit Krajeski <kitkrajeski@gmail.com>
2024-02-27 16:17:19 -0700

    business time

--- module-1/forception/main.js
+++ module-1/forception/main.js
@@ -1,36 +1,36 @@
-// var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-// var alphabet = "abcdefghijklmnopqrstuvwxyz"
-// alphabet = alphabet.toUpperCase()
-// console.log(alphabet);
-// var peopleAlpha = [];
-// for (var i = 0; i < people.length; i++) {
-//     peopleAlpha.push(people[i]);
-//     for (var j = 0; j < alphabet.length; j++) {
-//         peopleAlpha.push(alphabet[j]);
-//     }    
-// }
-// console.log(peopleAlpha)
-
-var cap = capitalize("hey friends! practice practice practice!") // -> "Hey Friends! Practice Practice Practice!"
-
-function capitalize(string) {
-    var characters = string.split("");
-    for (let i = 0; i < string.length; i++) {
-        if (string[i - 1] === " ") {
-            characters[i] = string[i].toUpperCase();
-        } 
-        if (i === 0) {
-            characters[i] = string[0].toUpperCase();
-        }
-    }
-    return characters.join("");
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = alphabet.toUpperCase()
+console.log(alphabet);
+var peopleAlpha = [];
+for (var i = 0; i < people.length; i++) {
+    peopleAlpha.push(people[i]);
+    for (var j = 0; j < alphabet.length; j++) {
+        peopleAlpha.push(alphabet[j]);
+    }    
 }
-console.log(cap);
+console.log(peopleAlpha)
 
-var age = 2
-age = 3
-console.log(age)
-age = 5
-var otherAge = age
-otherAge = 6
-console.log(otherAge)
+// var cap = capitalize("hey friends! practice practice practice!") // -> "Hey Friends! Practice Practice Practice!"
+
+// function capitalize(string) {
+//     var characters = string.split("");
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i - 1] === " ") {
+//             characters[i] = string[i].toUpperCase();
+//         } 
+//         if (i === 0) {
+//             characters[i] = string[0].toUpperCase();
+//         }
+//     }
+//     return characters.join("");
+// }
+// console.log(cap);
+
+// var age = 2
+// age = 3
+// console.log(age)
+// age = 5
+// var otherAge = age
+// otherAge = 6
+// console.log(otherAge)

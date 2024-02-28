98b20d5d631cc6ff2ef99b4d6dcd1ad00c02e72b
Kit Krajeski <kitkrajeski@gmail.com>
2024-02-28 15:43:10 -0700

    fizzbuzz counter update

--- module-1/sp73.5-fizzbuzz/main.js
+++ module-1/sp73.5-fizzbuzz/main.js
@@ -1,20 +1,25 @@
-let fizz = 0
-let buzz = 0
-let fizzbuzz = 0
+function Counter() {
+    this.fizz = 0;
+    this.buzz = 0;
+    this.fizzbuzz = 0;
+}
+const total = new Counter();
+// let fizz = 0
+// let buzz = 0
+// let fizzbuzz = 0
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("Fizzbuzz");
-        fizzbuzz ++
+        total.fizzbuzz ++
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        fizz++
+        total.fizz++
     } else if (i % 5 === 0) {
         console.log("Buzz");
-        buzz++
+        total.buzz++
     } else {
         console.log(i)
     }
 }
-console.log(fizzbuzz);
-console.log(fizz)
-console.log(buzz)
+
+console.log(total)
